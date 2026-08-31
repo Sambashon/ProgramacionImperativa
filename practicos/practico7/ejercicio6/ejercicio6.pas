fd14e0e5b4848dfed394af3adbcd4f2458ec05c9
Sebastian Fuenmayor <fuenmayorsebastiian@gmail.com>
2026-08-31 12:00:36 -0300

    no message

---
+++ practicos/practico7/ejercicio6/ejercicio6.pas
@@ -0,0 +1,46 @@
+{
+Como en el Ejercicio 5, implemente procedimientos con cabezales
+procedure diag1 (lado : integer; c1 , c2: char);
+procedure diag2 (lado : integer; c1 , c2: char);
+que al ser invocados con los argumentos 5, '*'y '.'desplieguen los cuadrados
+.****       ****.
+..***       ***..
+...**   y   **...
+....*       *....
+.....       .....
+}
+program ejercicio6;
+var
+    lado: integer;
+    c1, c2: char;
+procedure diag1 (lado : integer; c1 , c2: char);
+var
+    i, j: integer;
+begin
+    for i := 1 to lado do {salto de linea}
+    begin
+        for j := 1 to i do
+            write(c2);
+        for j := i + 1 to lado do
+            write(c1);
+
+        writeLn();
+    end;
+end;
+
+{
+procedure diag2 (lado : integer; c1 , c2: char);
+begin
+
+end;
+}
+
+begin
+    write('Ingrese lado: ');
+    readln(lado);
+    write('Ingrese c1 y c2: ');
+    readln(c1);
+    readln(c2);
+    writeLn();
+    diag1(lado, c1,c2);
+end.
